7d947c004cca1910731d0beb22ba92dd0aa5b234
Srinidhi Shankar Habbu <manusrinidhi10@gmail.com>
2017-11-19 01:29:26 -0500

    Update StockPredict.py

--- StockPredict.py
+++ StockPredict.py
@@ -1,27 +1,39 @@
 import csv
 import numpy as np
 from sklearn.svm import SVR
+import requests
 
 dates = []
 prices = []
 
 def get_data(filename):
-	with open(filename, 'r') as csvfile:
-		csvFileReader = csv.reader(csvfile)
-		next(csvFileReader)	# skipping column names
-		for row in csvFileReader:
-			dates.append(int(row[0].split('-')[0]))
-			prices.append(float(row[2]))
-	return
+    with open(filename, 'r') as csvfile:
+        csvFileReader = csv.reader(csvfile)
+        next(csvFileReader)	# skipping column names
+        c=0
+        for row in csvFileReader:
+            if c>25:
+                break
+            dates.append(int(row[0].split('-')[0]))
+            prices.append(float(row[2]))
+            c+=1
+        return
 
 def predict_price(dates, prices, x):
-	dates = np.reshape(dates,(len(dates), 1)) # converting to matrix of n X 1
+    dates = np.reshape(dates,(len(dates), 1)) # converting to matrix of n X 1
+    svr_rbf = SVR(kernel= 'rbf', C= 1e3, gamma= 0.1) # defining the support vector regression models
+    svr_rbf.fit(dates, prices) # fitting the data points in the models
+    return svr_rbf.predict(x)[0]
 
-	svr_rbf = SVR(kernel= 'rbf', C= 1e3, gamma= 0.1) # defining the support vector regression models
-	svr_rbf.fit(dates, prices) # fitting the data points in the models
+def get_historical(quote):
+    url = 'http://www.google.com/finance/historical?q=NASDAQ%3A'+quote+'&output=csv'
+    r = requests.get(url, stream=True)
+    if r.status_code != 400:
+        with open(quote+".csv", 'wb') as f:
+            for chunk in r:
+                f.write(chunk)
+    return True
 
-	return svr_rbf.predict(x)[0]
-
-get_data('goog2.csv') # calling get_data method by passing the csv file to it
-
-print(predict_price(dates, prices, 17))
+get_historical("mchp")
+get_data("mchp"+".csv") # calling get_data method by passing the csv file to it
+print(predict_price(dates, prices, 18))
