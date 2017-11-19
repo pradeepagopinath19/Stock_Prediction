98c64a1cc58c1fc409732b9f955f457854451ddb
Pradeepa Gopinath <pradeepagopinath19@gmail.com>
2017-11-19 00:56:55 -0500

    Update scrapeGoogleFinanceNews.py

--- scrapeGoogleFinanceNews.py
+++ scrapeGoogleFinanceNews.py
@@ -2,17 +2,19 @@ import requests
 import xml.etree.ElementTree as ET
 from textblob import TextBlob
 
-reqdoc = requests.get("https://finance.google.com/finance/company_news?q=NASDAQ:GOOGL&ei=E_0QWon_No6besrVqJgN&output=rss")
-positivity=0
-opinion=0
-noOfnews=0
-root = ET.fromstring(reqdoc.text)
-for n in root.iter('title'):
-    print(n.text)
-    noOfnews+=1
-    text = TextBlob(n.text)
-    factor = TextBlob(n.text).sentiment
-    opinion += factor.subjectivity
-    positivity += factor.polarity
-finalfactor = (opinion / noOfnews) * (positivity / noOfnews)
-print ("Final factor",finalfactor)
+def googleFinance(symbol):
+    reqdoc = requests.get("https://finance.google.com/finance/company_news?q=NASDAQ:"+symbol+"&ei=E_0QWon_No6besrVqJgN&output=rss")
+    positivity=0
+    opinion=0
+    noOfnews=0
+    root = ET.fromstring(reqdoc.text)
+    for n in root.iter('title'):
+        print(n.text)
+        noOfnews+=1
+        text = TextBlob(n.text)
+        factor = TextBlob(n.text).sentiment
+        opinion += factor.subjectivity
+        positivity += factor.polarity
+    finalfactor = ((opinion/8) / noOfnews) * (positivity*2/ noOfnews)
+    print ("Final factor",finalfactor)
+googleFinance("GOOGL")
